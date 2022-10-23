feacafc33d044c099ba7978cc0fd5b6183c4ae8f
lotsman1 <lotsman1@gmail.com>
2022-10-24 00:54:21 +0500

    Обновить pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+let i = 0
+basic.showLeds(`
+    . . . . #
+    . # . # .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
-	
+    i = 0
+    for (let index = 0; index < 10; index++) {
+        basic.showNumber(i)
+        i += 1
+        basic.pause(100)
+    }
 })
