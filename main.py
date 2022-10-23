0611fe37bc00d3a4d6ffa061e9f7a5a509031001
lotsman1 <lotsman1@gmail.com>
2022-10-24 01:08:16 +0500

    Обновить pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+i = 0
+basic.show_leds("""
+    . . . . #
+        . # . # .
+        . . # . .
+        . . . . .
+        . . . . .
+""")
+
 def on_forever():
-    pass
+    global i
+    i = 0
+    for index in range(10):
+        basic.show_number(i)
+        i += 1
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 let i = 0
 basic.showLeds(`
     . . . . #
-    . # . # .
-    . . # . .
-    . . . . .
-    . . . . .
-    `)
-basic.forever(function () {
+        . # . # .
+        . . # . .
+        . . . . .
+        . . . . .
+`)
+basic.forever(function on_forever() {
+    
     i = 0
     for (let index = 0; index < 10; index++) {
         basic.showNumber(i)
         i += 1
-        basic.pause(100)
     }
 })
